df6a3e943fb9cfa75783c731ba5933a85b19cc70
shruti248 <shrutiunadkat5@gmail.com>
2024-01-10 11:40:00 +0530

    Modified the endpoint to redirect it to google sign in

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 require('dotenv').config();
-const express = require ('express');
+const express = require('express');
+const utils = require('./utils')
 
 const app = express();
 
-app.get ('/auth', async (req, res) => {
-  res.send("auth route")
+
+// In our /auth endpoint, we redirect the user to the authorization server using the URL we imported earlier, save the code and restart the server.
+app.get('/auth', async (req, res) => {
+    //   res.send("auth route")
+    try {
+        res.redirect(utils.request_get_auth_code_url);
+    } catch (error) {
+        res.sendStatus(500);
+        console.log(error.message);
+    }
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
     console.log(`App is listening on http://localhost:${PORT}`)
 })
